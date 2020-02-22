05a153dc1ebdfc283f9ec778aa870ee3a7a79817
Soriety <dav.walkow@gmail.com>
2020-02-22 12:33:04 +1100

    Added proper electron stuff :0

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//idk
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('https://trello.com');
+
+  win.webContents.openDevTools(); //load devtools (dev only)
+}
+
+app.whenReady().then(createWindow);
+
+/* MACOS STANDARDS */
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
+/*                 */
