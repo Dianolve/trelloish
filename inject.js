a5c8eb0495171cc81f3e122db72e4b098fbeb63d
Soriety <dav.walkow@gmail.com>
2020-02-25 21:24:48 +1100

    ayyyy

--- inject.js
+++ inject.js
@@ -1,31 +1,36 @@
-const { remote, ipcRenderer } = require("electron");
-window.ipcRenderer = ipcRenderer;
-var content = remote.getCurrentWebContents();
+const { ipcRenderer } = require('electron');
 
-content.addListener("dom-ready", function() {
-    injectSettings();
-});
+function addStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
 
-content.addListener("will-navigate", function() {
-    injectSettings();
-});
-
+function darkMode() {
+    var background = document.getElementById('body'); //set #1c1e1f, set fff
+    var bottomGradient = document.getElementById('bottom-gradient'); //-webkit-gradient(linear, left top, left bottom, from(#1c1e1f), color-stop(46%, #1c1e1f), color-stop(74%, #1c1e1f), to(#1c1e1f))
+    var languagepicker = document.getElementById('language-picker'); //set #1c1e1f, set fff
+    var arrowleft = document.getElementsByClassName('arrow.arrow-left'); //
+    var quicksignup = document.getElementsByClassName('quick-signup'); //.atlassian-brand
+    var atlassianbrand = document.getElementsByClassName('atlassian-brand'); //inner-section .account-form
+    var innersection = document.getElementsByClassName('inner-section');
+    var accountform = document.getElementsByClassName('account-form');
+    var formfield = document.getElementsByClassName('form-field'); //.atlassian-brand .inner-section .form-field google-button
+    var googlebutton = document.getElementsByClassName('google-button');
+}
 
 function injectSettings() {
-    var menu = `<i id="settingslogo" class="material-icons">settings</i>`;
-    console.log('injected');
-    content.executeJavaScript(`
+    var menu = '<i id="settingslogo" class="material-icons">settings</i>';
     var materialIcons = document.createElement('link');
     materialIcons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
     materialIcons.setAttribute('rel', 'stylesheet');
     document.body.prepend(materialIcons);
     var menuDiv = document.createElement("div");
     menuDiv.setAttribute('id', 'settingsMenu');
-    menuDiv.addEventListener('click', function() { ipcRenderer.send('show-settings', true); } )
-    menuDiv.innerHTML = '${menu}';
-    document.body.prepend(menuDiv);;
-    `);
-    content.insertCSS(`
+    menuDiv.addEventListener('click', function() { ipcRenderer.send('show-settings', true); } );
+    menuDiv.innerHTML = menu;
+    document.body.prepend(menuDiv);
+    addStyleString(`
     @keyframes fadeOut {
         0% { opacity: 1;}
         100% { opacity: 0.1;}
@@ -44,6 +49,7 @@ function injectSettings() {
         color:white;
         animation: fadeOut 1s;
         opacity: 0.1;
+        z-indes: 999;
     }
     #settingslogo:hover {
         cursor: pointer;
@@ -51,5 +57,14 @@ function injectSettings() {
         animation: fadeIn 1s;
         opacity: 1;
     }
-    `);
+  `);
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    injectSettings();
+    const settingsDiv = document.querySelector('<?>');
+    settingsDiv.addEventListener('click', () => { 
+        ipcRenderer.send('test', 'ayy'); 
+    });
+});
+
